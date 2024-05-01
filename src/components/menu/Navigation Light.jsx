cebf1a01c1ba02ecb1d726d4653b5ec5c0af2371
Jean Trujillo <00zacks00@gmail.com>
2024-05-01 10:04:29 -0500

    Numero de productos en el carrito agregado al menu

--- src/components/menu/Navigation Light.jsx
+++ src/components/menu/Navigation Light.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { Fragment } from 'react'
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -11,78 +12,91 @@ const navigation = [
 
 export default function Navigation({ currentPage }) {
 
-  return (
-<Disclosure as="nav">
-  {({ open }) => (
-    <>
-      <div className="mx-auto px-5 xl:px-64 sm:py-2 bg-zinc-900">
-        <div className="relative flex h-16 items-center justify-between">
-          {/* Logo / Image */}
-          <div className="flex items-center gap-x-5">
-            <img
-              className="h-10 w-auto"
-              src="/procure.svg"
-              alt="Your Company"
-            />
-            <p className='font-semibold text-white'>@MiTienda</p>
-          </div>
+  let [cartSize, setCartSize] = useState(0)
 
-          {/* Mobile menu button*/}
-          <div className="absolute inset-y-0 right-0 flex items-center space-x-2 sm:hidden">
-              <div className='grid items-center'>
-                <a href="/checkout/" arial-label = "Icono de carrito de compras que redirecciona al checkout"><img src="/shopping-cart.svg" className='h-8 w-auto' alt="" /></a>
+  useEffect(() => {
+      const storedCart = JSON.parse(localStorage.getItem('cart')) || [];
+      const size = Object.keys(storedCart).length;
+      setCartSize(size);
+  }, []);
+
+  return (
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="mx-auto px-5 xl:px-64 sm:py-2 bg-zinc-900">
+            <div className="relative flex h-16 items-center justify-between">
+              {/* Logo / Image */}
+              <div className="flex items-center gap-x-5">
+                <img
+                  className="h-10 w-auto"
+                  src="/procure.svg"
+                  alt="Your Company"
+                />
+                <p className='font-semibold text-white'>@MiTienda</p>
               </div>
 
-            <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              {open ? (
-                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-              ) : (
-                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-              )}
-            </Disclosure.Button>
+              {/* Mobile menu button*/}
+              <div className="absolute inset-y-0 right-0 flex items-center space-x-2 sm:hidden">
+                <div className='relative grid items-center'>
+                  <a href="/checkout/" aria-label="Icono de carrito de compras que redirecciona al checkout">
+                    <img src="/shopping-cart.svg" className='h-8 w-auto' alt="" />
+                  </a>
+                  <a href="/checkout/" className='absolute bottom-0 right-0 bg-purple-50 rounded-full py-1 px-2 text-xs font-semibold text-purple-400'>{cartSize}</a>
+                </div>
+
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+              {/* Menu items */}
+              <div className="hidden sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={`rounded-md px-3 py-2 font-medium ${currentPage === item.href ? 'bg-pink-400 text-white' : 'text-white'}`}
+                      aria-current={currentPage === item.href ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                  <div className='relative items-center px-1'>
+                    <a href="/checkout/" aria-label="Icono de carrito de compras que redirecciona al checkout">
+                      <img src="/shopping-cart.svg" className='h-8 w-auto' alt="" />
+                    </a>
+                    <a href="/checkout/" className='absolute bottom-0 right-0 bg-purple-50 rounded-full py-1 px-2 text-xs font-semibold text-purple-400'>{cartSize}</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
-          {/* Menu items */}
-          <div className="hidden sm:block">
-            <div className="flex space-x-4">
+          {/* Responsive menu */}
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <a
                   key={item.name}
                   href={item.href}
-                  className={`rounded-md px-3 py-2 font-medium ${currentPage === item.href ? 'bg-pink-400 text-white' : 'text-white'}`}
+                  className={`block rounded-md px-3 py-2 text-base font-medium ${currentPage === item.href ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-500 hover:text-white'}`}
                   aria-current={currentPage === item.href ? 'page' : undefined}
                 >
                   {item.name}
                 </a>
               ))}
-              <div className='grid items-center'>
-                  <a href="/checkout/" arial-label = "Icono de carrito de compras que redirecciona al checkout"><img src="/shopping-cart.svg" className='h-8 w-auto' alt="" /></a>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Responsive menu */}
-      <Disclosure.Panel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className={`block rounded-md px-3 py-2 text-base font-medium ${currentPage === item.href ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-500 hover:text-white'}`}
-              aria-current={currentPage === item.href ? 'page' : undefined}
-            >
-              {item.name}
-            </a>
-          ))}
-        </div>
-      </Disclosure.Panel>
-    </>
-  )}
-</Disclosure>
-
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
