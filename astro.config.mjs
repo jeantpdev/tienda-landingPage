930530fd09ece5cbc951a79a635f252767da22a5
Jean Trujillo <00zacks00@gmail.com>
2024-05-07 23:32:25 -0500

    SSR Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
