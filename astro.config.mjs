d73b0f7e38ec9d270259897cf205dfe05a1adab3
Jean Trujillo <00zacks00@gmail.com>
2024-05-07 23:43:21 -0500

    SSR hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
+  output: "hybrid",
   adapter: netlify()
 });
