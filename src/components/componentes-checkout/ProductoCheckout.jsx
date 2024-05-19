9d9f45ef57eec7c33865ce5023e6188563deb926
Jean Trujillo <00zacks00@gmail.com>
2024-05-18 22:39:18 -0500

    Ahora se pueden enviar los pedidos realizados

--- src/components/componentes-checkout/ProductoCheckout.jsx
+++ src/components/componentes-checkout/ProductoCheckout.jsx
@@ -8,11 +8,10 @@ export default function ProductoCheckout({children, props}) {
     <div className='flex justify-between items-start'>
         <div className='flex gap-5'>
             <figure>
-                <img src={props.imagen_principal} className="h-20" alt={props.nombre_producto} />
+                <img src={props.imagen_principal} className="h-20 w-20 object-cover" alt={props.nombre_producto} />
             </figure>
             <div className="flex flex-col">
                 <p className='font-semibold tracking-tight leading-tight'>{props.nombre_producto}</p>
-                <p className='text-sm text-slate-500'>Material: {props.material}</p>
                 <p className='text-sm text-slate-500'>x {props.cantidad}</p>
                 <p className='text-sm text-slate-500'>{calcularPrecioConDescuentoYCantidad(props.precio, props.cantidad, props.descuento)}</p>
                 {props.descuento ? <p className='text-xs text-slate-500 line-through'>{calcularPrecioXCantidad(props.precio, props.cantidad)}</p> : ""}
